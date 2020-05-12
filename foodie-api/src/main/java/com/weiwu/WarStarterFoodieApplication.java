aa746a543166f96d35996f68db1548d6fcb58f83
weiwu0904 <weiwu0904@163.com>
2020-05-12 15:59:10 +0800

    feat: 修改

--- foodie-api/src/main/java/com/weiwu/WarStarterFoodieApplication.java
+++ foodie-api/src/main/java/com/weiwu/WarStarterFoodieApplication.java
@@ -1,12 +1,7 @@
 package com.weiwu;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import tk.mybatis.spring.annotation.MapperScan;
 
 public class WarStarterFoodieApplication extends SpringBootServletInitializer {
 
